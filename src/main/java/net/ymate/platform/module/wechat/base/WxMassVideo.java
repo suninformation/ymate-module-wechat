a64c108922a3235aea034fa906ba54b8bb8c815b
suninformation <suninformation@163.com>
2014-10-23 01:58:13 +0800

    补：微信接口更新：转短链接、上传群发视频、设置用户备注名等接口，同时为群发图文对象增加“是否显示封面”属性

---
+++ src/main/java/net/ymate/platform/module/wechat/base/WxMassVideo.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2007-2107 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.ymate.platform.module.wechat.base;
+
+import com.alibaba.fastjson.JSONObject;
+
+/**
+ * <p>
+ * WxMassVideo
+ * </p>
+ * <p>
+ * <p/>
+ *
+ * </p>
+ *
+ * @author 刘镇(suninformation@163.com)
+ * @version 0.0.0
+ *          <table style="border:1px solid gray;">
+ *          <tr>
+ *          <th width="100px">版本号</th><th width="100px">动作</th><th
+ *          width="100px">修改人</th><th width="100px">修改时间</th>
+ *          </tr>
+ *          <!-- 以 Table 方式书写修改历史 -->
+ *          <tr>
+ *          <td>0.0.0</td>
+ *          <td>创建类</td>
+ *          <td>刘镇</td>
+ *          <td>2014年10月23日上午1:41:26</td>
+ *          </tr>
+ *          </table>
+ */
+public class WxMassVideo {
+
+    private String mediaId;
+
+    private String title;
+
+    private String description;
+
+    public String getMediaId() {
+        return mediaId;
+    }
+
+    public void setMediaId(String mediaId) {
+        this.mediaId = mediaId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String toJSON() {
+        JSONObject _json = new JSONObject();
+        _json.put("media_id", this.getMediaId());
+        _json.put("title", this.getTitle());
+        _json.put("description", this.getDescription());
+        return _json.toString();
+    }
+
+}
