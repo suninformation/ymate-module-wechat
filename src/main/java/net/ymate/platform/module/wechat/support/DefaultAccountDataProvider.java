6d1b06df62d0d3d9db8461bbec02d896893577c0
suninformation <suninformation@163.com>
2016-07-23 16:41:47 +0800

    优化AccessToken和JsTicket缓存更新同步逻辑, 此版本已完成其实命宣告退役, 新版即将发布

--- src/main/java/net/ymate/platform/module/wechat/support/DefaultAccountDataProvider.java
+++ src/main/java/net/ymate/platform/module/wechat/support/DefaultAccountDataProvider.java
@@ -20,11 +20,14 @@ import net.ymate.platform.module.wechat.AccountDataMeta;
 import net.ymate.platform.module.wechat.IAccountDataProvider;
 import net.ymate.platform.module.wechat.WeChat;
 import net.ymate.platform.module.wechat.WeChat.WX_API;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 import java.util.Collections;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * <p>
@@ -52,15 +55,11 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 public class DefaultAccountDataProvider implements IAccountDataProvider {
 
-//	private static final Log _LOG = LogFactory.getLog(DefaultAccountDataProvider.class);
-
-//	private static Map<String, Object> __CACHES = new ConcurrentHashMap<String, Object>();
+    private static final Log _LOG = LogFactory.getLog(DefaultAccountDataProvider.class);
 
     protected static Map<String, AccountDataMeta> __accountCaches = new ConcurrentHashMap<String, AccountDataMeta>();
 
-    private static Object __LOCK = new Object();
-
-    private static Object __JS_TICKET_LOCK = new Object();
+    private static ConcurrentHashMap<String, ReentrantLock> __LOCK_MAP = new ConcurrentHashMap<String, ReentrantLock>();
 
     /**
      * 构造器
@@ -112,39 +111,81 @@ public class DefaultAccountDataProvider implements IAccountDataProvider {
      * @see net.ymate.platform.module.wechat.IAccountDataProvider#getAccessToken(java.lang.String)
      */
     public String getAccessToken(String accountId) throws Exception {
-        AccountDataMeta _accessToken = null;
-        synchronized (__LOCK) {
-            _accessToken = __accountCaches.get(accountId);
-            //
-            long _currentTime = System.currentTimeMillis();
-            if (_accessToken == null || (_currentTime >= _accessToken.getAccessTokenExpires())) {
-                // {"access_token":"ACCESS_TOKEN","expires_in":7200}
-                JSONObject _tokenJSON = WeChat.__doCheckJsonResult(HttpClientHelper.create().doGet(WX_API.WX_ACCESS_TOKEN.concat("&appid=") + getAppId(accountId) + "&secret=" + getAppSecret(accountId)));
-                _accessToken.setAccessToken(_tokenJSON.getString("access_token"));
-                _accessToken.setAccessTokenExpires(_currentTime + _tokenJSON.getIntValue("expires_in") * 1000);
-                //
-//				_LOG.debug("AccessToken Has Expired, Get From Remote: " + _accessToken.getAccountId());
-//			} else {
-//				_LOG.debug("Get AccessToken From Cache: " + _accessToken);
+        AccountDataMeta _accessToken = __accountCaches.get(accountId);
+        if (_accessToken == null || (System.currentTimeMillis() >= _accessToken.getAccessTokenExpires())) {
+            ReentrantLock _locker = __LOCK_MAP.get(accountId + "_accesstoken");
+            if (_locker == null) {
+                _locker = new ReentrantLock();
+                ReentrantLock _previous = __LOCK_MAP.putIfAbsent(accountId + "_accesstoken", _locker);
+                if (_previous != null) {
+                    _locker = _previous;
+                }
             }
+            _locker.lock();
+            //
+            try {
+                long _currentTime = System.currentTimeMillis();
+                if (_accessToken == null || (_currentTime >= _accessToken.getAccessTokenExpires())) {
+                    // {"access_token":"ACCESS_TOKEN","expires_in":7200}
+                    JSONObject _tokenJSON = null;
+                    try {
+                        _tokenJSON = WeChat.__doCheckJsonResult(HttpClientHelper.create().doGet(WX_API.WX_ACCESS_TOKEN.concat("&appid=") + getAppId(accountId) + "&secret=" + getAppSecret(accountId)));
+                    } catch (Exception e) {
+                        _LOG.debug("AccessToken Exception: ", e);
+                        _tokenJSON = WeChat.__doCheckJsonResult(HttpClientHelper.create().doGet(WX_API.WX_ACCESS_TOKEN.concat("&appid=") + getAppId(accountId) + "&secret=" + getAppSecret(accountId)));
+                    }
+                    _LOG.debug("AccessToken Result: " + _tokenJSON.toJSONString());
+                    _accessToken.setAccessToken(_tokenJSON.getString("access_token"));
+                    _accessToken.setAccessTokenExpires(_currentTime + _tokenJSON.getIntValue("expires_in") * 1000);
+                    //
+                    _LOG.debug("AccessToken Has Expired, Get " + _accessToken.getAccountId() + " From Remote: " + _accessToken.getAccessToken());
+                }
+            } finally {
+                _locker.unlock();
+            }
+        } else {
+            _LOG.debug("Get " + _accessToken.getAccountId() + " AccessToken From Cache: " + _accessToken.getAccessToken());
         }
         return _accessToken.getAccessToken();
     }
 
     public String getJsApiTicket(String accountId) throws Exception {
-        AccountDataMeta _account = null;
-        synchronized (__JS_TICKET_LOCK) {
-            _account = __accountCaches.get(accountId);
-            //
-            long _currentTime = System.currentTimeMillis();
-            if (_account == null || (_currentTime >= _account.getJsAPiTicketExpires())) {
-                // { "errcode":0, "errmsg":"ok", "ticket":"bxLdikRXVbTPdHSM05e5u5sUoXNKd8-41ZO3MhKoyN5OfkWITDGgnr2fwJ0m9E8NYzWKVZvdVtaUgWvsdshFKA", "expires_in":7200 }
-                JSONObject _ticketJSON = WeChat.__doCheckJsonResult(HttpClientHelper.create().doGet(WX_API.OAUTH_JSAPI_TICKET.concat(getAccessToken(accountId))));
-                _account.setJsApiTicket(_ticketJSON.getString("ticket"));
-                _account.setJsAPiTicketExpires(_currentTime + _ticketJSON.getIntValue("expires_in") * 1000);
+        AccountDataMeta _accessToken = __accountCaches.get(accountId);
+        if (_accessToken == null || (System.currentTimeMillis() >= _accessToken.getAccessTokenExpires())) {
+            ReentrantLock _locker = __LOCK_MAP.get(accountId + "_jsapitoken");
+            if (_locker == null) {
+                _locker = new ReentrantLock();
+                ReentrantLock _previous = __LOCK_MAP.putIfAbsent(accountId + "_jsapitoken", _locker);
+                if (_previous != null) {
+                    _locker = _previous;
+                }
             }
+            _locker.lock();
+            //
+            try {
+                long _currentTime = System.currentTimeMillis();
+                if (_accessToken == null || (_currentTime >= _accessToken.getJsAPiTicketExpires())) {
+                    // { "errcode":0, "errmsg":"ok", "ticket":"bxLdikRXVbTPdHSM05e5u5sUoXNKd8-41ZO3MhKoyN5OfkWITDGgnr2fwJ0m9E8NYzWKVZvdVtaUgWvsdshFKA", "expires_in":7200 }
+                    JSONObject _ticketJSON = null;
+                    try {
+                        _ticketJSON = WeChat.__doCheckJsonResult(HttpClientHelper.create().doGet(WX_API.OAUTH_JSAPI_TICKET.concat(getAccessToken(accountId))));
+                    } catch (Exception e) {
+                        _LOG.debug("JsApiToken Exception: ", e);
+                        _ticketJSON = WeChat.__doCheckJsonResult(HttpClientHelper.create().doGet(WX_API.OAUTH_JSAPI_TICKET.concat(getAccessToken(accountId))));
+                    }
+                    _LOG.debug("JsApiToken Result: " + _ticketJSON.toJSONString());
+                    _accessToken.setJsApiTicket(_ticketJSON.getString("ticket"));
+                    _accessToken.setJsAPiTicketExpires(_currentTime + _ticketJSON.getIntValue("expires_in") * 1000);
+                    //
+                    _LOG.debug("JsApiToken Has Expired, Get " + _accessToken.getAccountId() + " From Remote: " + _accessToken.getJsApiTicket());
+                }
+            } finally {
+                _locker.unlock();
+            }
+        } else {
+            _LOG.debug("Get " + _accessToken.getAccountId() + " JsApiToken From Cache: " + _accessToken.getJsApiTicket());
         }
-        return _account.getJsApiTicket();
+        return _accessToken.getJsApiTicket();
     }
 
     /* (non-Javadoc)
